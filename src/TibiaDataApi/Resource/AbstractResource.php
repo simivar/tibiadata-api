740976258888c09358c35840c66a9cb194eefa73
Simivar <simivar@gmail.com>
2018-05-18 16:57:09 +0200

    Basic resource

---
+++ src/TibiaDataApi/Resource/AbstractResource.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace TibiaDataApi\Resources;
+
+use TibiaDataApi\TibiaData;
+
+class AbstractResource
+{
+    
+    const API_URL = 'v2/';
+
+    /** @var TibiaData */
+    protected $tibiaData;
+    
+    public function __construct(TibiaData $tibiaData)
+    {
+        $this->tibiaData = $tibiaData;
+    }
+    
+    protected function sendRequest(
+        $method,
+        $uri,
+        array $headers = [],
+        $body = null,
+        $protocolVersion = '1.1'
+    ): \stdClass {
+        $req =  $this->tibiaData->getRequestFactory()->createRequest($method, self::API_URL . $uri, $headers, $body, $protocolVersion);
+        
+        return json_decode($this->tibiaData->getHttpClient()->sendRequest($req)->getBody());
+    }
+    
+}
